ab950c18e58e70e7510adc03beca72048636ba1c
Isaac Lum <zisaac2468@gmail.com>
2024-06-09 01:11:00 +0800

    Added files for MOOC FI Java Part 4 Introduction to object-orientated programming

--- Playground/sandbox.py
+++ Playground/sandbox.py
@@ -1,5 +1,19 @@
-x = 500
+rocks = 11
+arr = [30,70, 75, 120, 160, 170, 180, 190, 200, 246, 258]
+hop = 50
+next = 0
+curr = 0
+counter = 0
 
-s = "123131"
-for i in x:
-    print(i)
+
+while(next <= rocks - 1):
+    if arr[next] - curr > hop:
+        curr = arr[next - 1]
+        counter += 1
+    else:
+        next += 1
+
+counter += 1
+print(counter)
+
+        
--- Random/MOOC FI Java/Part 4/introOOP.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class introOOP{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Test");
-    }
-}
